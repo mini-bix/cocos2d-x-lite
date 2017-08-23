afb3bb191d882d458ed0b377dce2d98568657595
gumimin_175 <gumimin_175@qq.com>
2017-08-23 18:00:41 +0800

    revert

--- revert_appstore.sh
+++ revert_appstore.sh
@@ -2,4 +2,4 @@ cp -f anysdk_appstore/src_ori/* /Applications/CocosCreator.app/Contents/Resource
 git checkout -- extensions/anysdk/js-bindings
 python download-deps.py
 BUILD=`../client/builddir.sh`
-cp -rf extensions/anysdk/js-bindings_ori/* ../client/"$BUILD"frameworks/runtime-src/Classes
+cp -rf anysdk_appstore/js-bindings_ori/* ../client/"$BUILD"frameworks/runtime-src/Classes
